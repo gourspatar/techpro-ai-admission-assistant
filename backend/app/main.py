ac6e05df79a4ac4adb0472f597d8afc17915f771
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-30 10:53:33 +0530

    chore: initialize FastAPI backend

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="TechPro AI Admission Assistant",
+    version="1.0.0",
+)
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "healthy"
+    }
